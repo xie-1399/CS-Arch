5c1c4186ba590ed6b63b3504ecb61e040ab4caed
xxl <3161932956@qq.com>
2023-05-11 16:27:47 +0800

    update NJU OS 6 about Thread

--- C++/Pointer/Basic.cpp
+++ C++/Pointer/Basic.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 int *swap(int *px , int *py){
     int temp;
@@ -15,6 +16,28 @@ void get_char_while(){
     }
 }
 
+/*
+ * 可变的函数参数列表,但是好像顺序不太对的颜值高i
+ */
+double average(int num,...)
+{
+
+    va_list valist;
+    double sum = 0.0;
+    int i;
+
+    /* 为 num 个参数初始化 valist */
+    va_start(valist, num);
+    /* 访问所有赋给 valist 的参数 */
+    for (i = 0; i < num; i++)
+    {
+        printf("number : %d\n",va_arg(valist,int));
+        sum += va_arg(valist, int);
+    }
+    /* 清理为 valist 保留的内存 */
+    va_end(valist);
+    return sum/num;
+}
 
 
 int main(int argc,char *argv[]){
@@ -31,12 +54,10 @@ int main(int argc,char *argv[]){
     int a[10] = {0};
     printf("%d\n",a[2]);  // 2[a] also can work
 
-    for(int  q = 0 ; argv[q] != NULL ; q++){
-        printf("argv[%d] = %s\n",q,argv[q]);
-    }
-    get_char_while();
-
-
-
+//    for(int  q = 0 ; argv[q] != NULL ; q++){
+//        printf("argv[%d] = %s\n",q,argv[q]);
+//    }
+    //get_char_while();
+    average(3,1,2,3);
     return 0;
 }
---
+++ OS/NJU/Thread/Lock/xchg.c
@@ -0,0 +1,32 @@
+/*
+ * 使用原子指令来实现自旋锁
+ */
+
+//根据标记进行判断
+#include "../thread.h"
+
+#define N 100000000
+
+int cas(int *addr,int cmp_val,int new_val){
+    int old_val = *addr;
+    if(old_val == cmp_val){
+        *addr = new_val;
+        return 0;
+    } else{
+        return 1;
+    }
+}
+long sum = 0;
+
+void Tsum() {
+    for (int i = 0; i < N; i++) {
+        asm volatile("lock addq $1, %0": "+m"(sum));
+    }
+}
+
+int main() {
+    create(Tsum);
+    create(Tsum);
+    join();
+    printf("sum = %ld\n", sum);
+}
